82e16b420c25704be0602a87e4d7fbced82f8c28
joshuaakhimiemona <joshuaakhimiemona@172.17.6.234>
2022-08-30 14:06:58 -0400

    This is my commit for assignment #1

---
+++ lsp-assignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+
+/*
+ * @author Joshua Akhimiemona @02968958
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!");
+
+	}
+
+}
